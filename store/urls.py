b3bce9b149aca5d7c54dc38d7803a76eb62b93f7
Joe <joseph.lee3454@gmail.com>
2021-03-04 13:38:54 -0800

    able to add to cart

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path , include
 from . import views
+
 urlpatterns = [
-    path('',views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/',views.checkout, name="checkout")
+
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+
 ]
