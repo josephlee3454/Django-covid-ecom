602de159fdec5704f8ef8a6803a1767e7984c54a
Joe <joseph.lee3454@gmail.com>
2021-03-04 10:57:18 -0800

    added images to store

--- store/models.py
+++ store/models.py
@@ -14,11 +14,20 @@ class Customer(models.Model):
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	price = models.CharField(max_length=20)
 	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
